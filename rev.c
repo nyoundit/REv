a66d52c7e551b6d5466561dccf470d52591f8cf2
nyoundit <nyoundit.m@gmail.com>
2018-08-09 17:06:01 +0300

    added the c file

---
+++ rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+//stores the  maximum amount of input words
+char word[50];
+//prompts users for input
+printf("Enter Your word in Capital letters\n");
+// waits for user input
+scanf("%s", word);
+//loops through the input
+for(int i = 0, wordLength = strlen(word); i < wordLength; i++) {
+//if the letter of the input is between the capital of a and z minus 32 from its ASCII code
+    if(word[i] >= 'A' && word[i] <= 'Z') {
+        printf("%c translates to %c", word[i], word[i] - ('a' - 'A'));
+    } else if(word[i] >= 'a' && word[i] <= 'z'){
+        printf("%c translates to %c", word[i], word[i] - 64);
+    }
+    printf("\n");
+  }
+}

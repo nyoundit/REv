4daece2633b69a4ffd7ac70e544bc3e153d728ac
nyoundit <nyoundit.m@gmail.com>
2018-08-09 17:07:50 +0300

    made comments on the code

--- rev.c
+++ rev.c
@@ -13,6 +13,7 @@ for(int i = 0, wordLength = strlen(word); i < wordLength; i++) {
 //if the letter of the input is between the capital of a and z minus 32 from its ASCII code
     if(word[i] >= 'A' && word[i] <= 'Z') {
         printf("%c translates to %c", word[i], word[i] - ('a' - 'A'));
+//else if the letter of the input is between  a and z minus 64 from its ASCII code
     } else if(word[i] >= 'a' && word[i] <= 'z'){
         printf("%c translates to %c", word[i], word[i] - 64);
     }
